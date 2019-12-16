bbe6f8dee1938682fd2df24fe4dcd42beb218109
Hakim Ghods <ghods.hakim@hotmail.com>
2019-12-16 04:28:25 +0330

    Add project files.

---
+++ TitanicMLDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TitanicMLDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
